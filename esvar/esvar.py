b4940cecaf2d66943254afbaf7f0cad160274da1
sriva <sriva@cbrglogin1>
2024-06-18 14:34:44 +0100

    Adding description

--- esvar/esvar.py
+++ esvar/esvar.py
@@ -16,12 +16,34 @@ class esvar():
 	
 	def __init__(self, genetic=None, number_of_folds=5, output="output", genome="hg38", seed=42, collection_name=None):
 
+		"""\
+			__init__.
+
+			Parameters
+			----------
+			genetic : str
+				Path to genetic file.
+			number_of_folds : integer
+				Number of folds to create backgound using the 1000genomes.
+			output : str
+				Directory where to save the scores.
+			genome : str
+				Genome to use, available 'hg19' and 'hg38'.
+			seed : int
+				Seed for reproducibility, shuffle 1000genomes excluded.
+			collection_name : str
+				Data collection name, available 'cad', 'calderon', 'catlas_fetal', 'catlas_adult', 'erythoid_d7_d10_d13_d17', 'h1_hescs', 'immune_cell', 'ludwig2019', 'mpal', 'pancreatic_pbmc', and 'super_pbmc'
+
+		"""
+
 		self.seed = seed
 		if (self.seed != None) and (isinstance(self.seed , int)):
 			random.seed(self.seed)
 			np.random.seed(self.seed)
 
-		self.df_genetics = genetic
+		self.genetic_file = genetic
+
+		self.genetic_df = pd.DataFrame()
 
 		self.output = output
 		self.tmp = self.output+'/tmp'
@@ -33,13 +55,12 @@ class esvar():
 		if self.genome == "hg38":
 			self.mappable_bp = 3049315783 #https://genomewiki.ucsc.edu/index.php?title=Hg38_27-way_Genome_size_statistics
 			self.background = "https://datashare.molbiol.ox.ac.uk/public/project/Wellcome_Discovery/ESVAR_collection/1000genomes/ALL_1000_genomes.variants.hg38.bed"
-		# elif self.genome == "hg19":
+		elif self.genome == "hg19":
 		# 	self.mappable_bp = 2897310462 #https://genomewiki.ucsc.edu/index.php?title=Hg19_100way_Genome_size_statistics
 		# 	self.background = "https://datashare.molbiol.ox.ac.uk/public/project/Wellcome_Discovery/ESVAR_collection/1000genomes/ALL_1000_genomes.variants.hg19.bed"
+			sys.exit("Genome 'hg19' not functional at the moment. Please use genome 'hg38'.")
 		else:
 			sys.exit("Select genome between 'hg19' and 'hg38'.")
-
-		self.df_collection = pd.DataFrame()
 		
 		self.URL = "https://datashare.molbiol.ox.ac.uk/public/project/Wellcome_Discovery/ESVAR_collection"
 
@@ -66,19 +87,20 @@ class esvar():
 	def __check_genetic_format(self, df_genetics):
 
 		"""\
-			Check generic format.
+			Check genetic format.
 
 			Parameters
 			----------
 			df_genetics : pandas.DataFrame
-				Description
+				DataFrame of initial genetics.
 
 			Returns
 			-------
 			df_genetics : pandas.DataFrame
+				DataFrame of filtered genetics.
 		"""
 
-		if not os.path.exists(self.df_genetics):
+		if not os.path.exists(self.genetic_file):
 			sys.exit("Error, gentic file does not exist.")
 
 		print("Removing NaN rows from loaded file ...")
@@ -122,7 +144,22 @@ class esvar():
 
 
 	def __load_genetic(self, less100=False):
-		df_genetics = pd.read_csv(self.df_genetics, sep="\t")
+
+		"""\
+			Load genetic.
+
+			Parameters
+			----------
+			less100 : Boolean
+				Boolean variable to force the software to run also with less than 100 variants per file.
+
+			Returns
+			-------
+			df_genetics : pandas.DataFrame
+				DataFrame of filtered genetics.
+		"""
+
+		df_genetics = pd.read_csv(self.genetic_file, sep="\t")
 		df_genetics = self.__check_genetic_format(df_genetics)
 		df_genetics = df_genetics.dropna(subset=['CHR_ID'], axis=0)
 		df_genetics["CHR_ID"] = df_genetics["CHR_ID"].astype(str)
@@ -139,16 +176,45 @@ class esvar():
 		if df_genetics.shape[0]<100:
 			if less100 == False:
 				sys.exit("Genetics provided after quality control contains less than the minimum number (100 variants) of entries.\nIf you want to carry on anyway with it, please set 'less100=True'.")
+
+			self.genetic_df = df_genetics
 		return df_genetics
 
 
 	def __loading_info(self):
+
+		"""\
+			Load cell type information per collection type.
+
+			Returns
+			-------
+			info : pandas.DataFrame
+				DataFrame of cell type names per data collection.
+		"""
+
 		file = '%s/%s/%s_info.csv'%(self.URL, self.collection_name_dict[self.collection_name], self.collection_name_dict[self.collection_name])
 		info = pd.read_csv(file, sep='\t', names=['cellType','scCounts'])[['cellType']]
 		return info
 
 
 	def __loading_bigwigs_and_beds(self, info):
+
+		"""\
+			Load bigwig and bed files per data collection.
+
+			Parameters
+			----------
+			info : pandas.DataFrame
+				DataFrame of cell type names per data collection.
+
+			Returns
+			-------
+			bigwigs : list
+				List of bigwig files per datacollection ordered per cell type within data collection.
+			beds : list
+				List of beds files per datacollection ordered per cell type within data collection.
+		"""
+
 		bigwigs, beds = [], []
 		for ct in info['cellType'].tolist():
 			bigwigs.append("%s/%s/bigwigs/%s.bw"%(self.URL, self.collection_name_dict[self.collection_name], ct))		
@@ -157,12 +223,43 @@ class esvar():
 
 
 	def __loading_collection_data(self):
+
+		"""\
+			Load selected data collection.
+
+			Returns
+			-------
+			info : pandas.DataFrame
+				DataFrame of cell type names per data collection.
+			bigwigs : list
+				List of bigwig files per datacollection ordered per cell type within data collection.
+			beds : list
+				List of beds files per datacollection ordered per cell type within data collection.
+		"""
+
 		info = self.__loading_info()
 		bigwigs, beds = self.__loading_bigwigs_and_beds(info)
-		return info, beds, bigwigs
+		return info, bigwigs, beds
 
 
 	def __prepare_data(self, beds, df_genetics):
+
+		"""\
+			Check genetic format.
+
+			Parameters
+			----------
+			beds : list
+				List of beds files per datacollection ordered per cell type within data collection.
+			df_genetics : pandas.DataFrame
+				DataFrame of filtered genetics.
+
+			Returns
+			-------
+			df_data : pandas.DataFrame
+				DataFrame of initialised information for encrichment score calculation.
+		"""
+		
 		df_data = pd.DataFrame()
 		peak_area, ps, celltypes = [], [], []
 		print("Calculating (1) total number of base-pairs within peaks and (2) total number of base-pairs within peaks divided by uniquely mappable base-pairs ...")
@@ -202,6 +299,11 @@ class esvar():
 
 
 	def __download_background(self):
+
+		"""\
+			Download background from 1000genomes for enrichment score.
+		"""
+		
 		if not os.path.exists(self.folds):
 			os.makedirs(self.folds)
 		if not os.path.isfile(self.folds+os.sep+"ALL_1000_genomes.variants.%s.bed"%self.genome):
@@ -209,12 +311,33 @@ class esvar():
 			_ = subprocess.run('wget -P %s/ %s'%(self.folds, self.background), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
-	def _shuf(self, x):
-			return subprocess.run(x, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	def _shuf(self, command):
+
+		"""\
+			Subprocess for shuffling and extracting genetics from 1000genomes.
+
+			Parameters
+			----------
+			command : str
+				Bash command for shuffle and extract genetics.
+		"""
+		
+		return subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 	def __shuffle_background(self, number_of_genetic, idx):
 
+		"""\
+			Shuffle and extract genetics from 1000genomes.
+
+			Parameters
+			----------
+			number_of_genetic : int
+				Number of variant to extract from 1000genomes.
+			idx : int
+				Fold index for background extraction.
+		"""
+		
 		print("For each fold, parallelised shuffling background ...")
 
 		if not os.path.exists(self.folds+os.sep+"round%s"%idx):
@@ -228,12 +351,43 @@ class esvar():
 			_ = pool.map(self._shuf, commands)
 
 
-	def __create_background(self, number_of_genetic, idx): 
+	def __create_background(self, number_of_genetic, idx):
+
+		"""\
+			Create background for enrichment score.
+
+			Parameters
+			----------
+			number_of_genetic : int
+				Number of variant to extract from 1000genomes.
+			idx : int
+				Fold index for background extraction.
+		"""
+		
 		self.__download_background()
 		self.__shuffle_background(number_of_genetic, idx)
 		
 	
 	def __add_shuffled_background(self, df_data, beds, idx):
+
+		"""\
+			Load shuffled background to initilised enrichment score DataFrame.
+
+			Parameters
+			----------
+			df_data : pandas.DataFrame
+				DataFrame of initialised information for encrichment score calculation.
+			beds : list
+				List of beds files per datacollection ordered per cell type within data collection.
+			idx : pandas.DataFrame
+				Fold index for loading extracted background.
+
+			Returns
+			-------
+			df_data : pandas.DataFrame
+				DataFrame of initialised and background information for encrichment score calculation.
+		"""
+		
 		print("Adding shuffled background ...")
 		for idx_f, fold in enumerate(tqdm(glob.glob("%s/round%s/SUB*.bed"%(self.folds, idx)))):
 			df_fold = pd.read_csv(fold, sep="\t", header=None)[[0,1,2]]
@@ -254,6 +408,22 @@ class esvar():
 
 	def __add_statistics(self, df_data, number_of_genetic):
 
+		"""\
+			Add statisticts to .
+
+			Parameters
+			----------
+			df_data : pandas.DataFrame
+				DataFrame of initialised and background information for encrichment score calculation.
+			number_of_genetic : int
+				Number of variant to extract from 1000genomes.
+                
+			Returns
+			-------
+			df_data : pandas.DataFrame
+				Encrichment score DataFrame.
+		"""
+
 		print("Calculating statistics ...")
 
 		df_data["P_gwas"] = stats.binom.pmf(df_data["GWAS_init"], number_of_genetic, df_data["p_succes"])
@@ -268,17 +438,60 @@ class esvar():
 
 
 	def __clean_tmp(self):
+
+		"""\
+			Delete temporary files.
+		"""
+		
 		print("Cleaning temporary files ...")
 		shutil.rmtree(self.tmp)
 
 
 	def __parallel_background(self, idx, df_genetics):
+
+		"""\
+			Parallel computation for background estimation.
+
+			Parameters
+			----------
+			idx : pandas.DataFrame
+				Fold index for loading extracted background.
+			df_genetics : pandas.DataFrame
+				DataFrame of filtered genetics.
+
+			Returns
+			-------
+			df_genetics : pandas.DataFrame
+				DataFrame of filtered genetics.
+		"""
+		
 		number_of_genetic = df_genetics.shape[0]
 		self.__create_background(number_of_genetic, idx)
 		return idx, number_of_genetic
 
 
 	def _parallel_ces(self, idx, df_collection, number_of_genetic, beds):
+
+		"""\
+			Parallel computation for enrichment score calculation.
+
+			Parameters
+			----------
+			idx : pandas.DataFrame
+				Fold index to add background and enrichment score calculation per fold.
+			df_collection : pandas.DataFrame
+				Enrichment score DataFrame.
+			number_of_genetic : int
+				Number of variant to extract from 1000genomes.
+			beds : list
+				List of beds files per datacollection ordered per cell type within data collection..
+
+			Returns
+			-------
+			df_genetics : pandas.DataFrame
+				Description
+		"""
+		
 		df_collection = self.__add_shuffled_background(df_collection, beds, idx)
 		df_collection = self.__add_statistics(df_collection, number_of_genetic)
 		df_collection.to_csv(self.output+os.sep+"rounds"+os.sep+"statistics_intermediate_round%s.csv"%(idx+1), sep="\t")
@@ -286,7 +499,22 @@ class esvar():
 
 
 	def calculate_enrichment_score(self, less100=False):
-		if self.df_genetics is None:
+
+		"""\
+			Calculate enrichment score for provided genetics per selected data collection.
+
+			Parameters
+			----------
+			less100 : Boolean
+				Boolean variable to force the software to run also with less than 100 variants per file.
+
+			Returns
+			-------
+			dfs_collection : pandas.DataFrame
+				Final enrichment score DataFrame for provided genetics and selected data collection.
+		"""
+		
+		if self.genetic_file is None:
 			sys.exit("Error, missing genetic file.")
 		
 		df_genetics = self.__load_genetic(less100=less100)
@@ -306,7 +534,7 @@ class esvar():
 				df_genetics_round = df_genetics_rest.sample(sub_n, random_state=42)
 				df_genetics_list.append(df_genetics_round)
 
-		info, beds, _ = self.__loading_collection_data()
+		info, _, beds = self.__loading_collection_data()
 		idx_list, df_collection_list, number_of_genetic_list = [], [], []
 		for idx, df_genetic in enumerate(df_genetics_list):
 			df_collection = self.__prepare_data(beds, df_genetic)
@@ -330,7 +558,6 @@ class esvar():
 		dfs_collection = dfs_collection[['ESVAR_all']]
 
 		dfs_collection.to_csv(self.output+os.sep+"statistics_ESVAR.csv", sep="\t")
-		self.df_collection = dfs_collection
 
 		self.__clean_tmp()
 		print("Processing genetics finished.")
